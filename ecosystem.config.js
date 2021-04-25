6fdbd4eda8ba55a08bed81eb317e59e26c6c45fa
2langk <leelk980@gmail.com>
2021-04-25 16:37:56 +0900

    1234

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,11 +7,6 @@ module.exports = {
       instanses: 'max',
       exec_mode: 'cluster',
       env: {NODE_ENV: 'production'},
-      log_date_format: 'YYYY-MM-DD HH:mm:ss',
-      error_file: './logs/pm2-err.log',
-      out_file: './logs/pm2-out.log',
-      combine_logs: true,
-      merge_logs: true,
     },
   ],
 };
